651eb9abd93ee34e9fca76d6b279f50791b9e447
TitikUniverse <semyon.titov.203@mail.ru>
2021-05-18 19:26:18 +0300

    Game Over Change

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class GameOver : MonoBehaviour
 {
     public static GameOver Instanse;
     public GameObject gameOverDisplay;
+
+    public GameObject resultTextUI;
 
     public GameOver()
     {
@@ -16,6 +19,11 @@ public class GameOver : MonoBehaviour
 
     public void ShowGameOver()
     {
+        SetBestScore(GameManager.Instanse.Score);
+
+        resultTextUI.GetComponent<Text>().text = "—чет: " + GameManager.Instanse.Score + "\r\n" + "–екорд: " + GetBestScore().ToString();                                                                                                                                                                                                             
+
+
         gameOverDisplay.SetActive(true);
         Time.timeScale = 0;
         GameManager.Instanse.TouchDetectActive = false;
@@ -25,5 +33,18 @@ public class GameOver : MonoBehaviour
     {
         Time.timeScale = 1;
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public int GetBestScore()
+    {
+        return PlayerPrefs.GetInt("BestScore");
+    }
+
+    public void SetBestScore(int score)
+    {
+        if (score > PlayerPrefs.GetInt("BestScore"))
+        {
+            PlayerPrefs.SetInt("BestScore", score);
+        }
     }
 }
